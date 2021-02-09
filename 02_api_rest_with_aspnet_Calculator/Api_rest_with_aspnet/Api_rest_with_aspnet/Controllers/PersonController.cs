86811d4a31812b8ace7aa5f82d999d8f00ee8cc4
Diego Harrison <diegoharrisonsm@gmail.com>
2021-02-09 14:31:26 -0300

    create model person

---
+++ 02_api_rest_with_aspnet_Calculator/Api_rest_with_aspnet/Api_rest_with_aspnet/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Api_rest_with_aspnet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Invalid input");
+        }        
+    }
+}
